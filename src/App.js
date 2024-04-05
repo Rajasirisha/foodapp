67f73e6ad42c5423cd78cdef96312f136fe11959
Rajasirisha <sirisha.siri11@gmail.com>
2024-04-05 09:33:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div>
   <BrowserRouter>
   <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path="/home" element={Home} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         </Routes>
   </BrowserRouter>
 
